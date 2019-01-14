53ceff7ff7a04f5093cf246bdf18d0eb22af983d
Tristan <letristandu39@hotmail.fr>
2019-01-14 16:20:30 +0100

    [FIX] actu's route rss added

--- controller/actuController.js
+++ controller/actuController.js
@@ -1,9 +1,14 @@
 var request = require('request');
+var xml2js = require('xml2js');
+
+var parser = new xml2js.Parser();
 
 /* Get RSS Flux from AS Lyon 1 */
 exports.listActuRss = function(req, res) {
     request.get('https://as.univ-lyon1.fr/feed/', function(error, response, body) {
-        sendJsonResponse(res, 200, body);
+        //sendJsonResponse(res, 200, body);
+        var json = parser.parseString(body);
+        res.send(json);
     });
 };
 
