7d09585faa1aa359d12779b9c7db9271604fef52
Tristan <letristandu39@hotmail.fr>
2019-01-15 14:08:17 +0100

    [TASK] add title event for notify

--- controller/notificationController.js
+++ controller/notificationController.js
@@ -8,18 +8,25 @@ admin.initializeApp({
   databaseURL: "https://aslyon1-80c44.firebaseio.com"
 });
 
+var topic = "aslyon";
+
 //notification evenements
 function notificationEvent(eventTitle) {
   var payload = {
     notification: {
       title: "Nouvel évènement",
       body: eventTitle
+    },
+    data: {
+      idEvent: title
     }
   };
   
-  var topic = "aslyon";
+  var options = {
+    priority: "high"
+  }
   
-  admin.messaging().sendToTopic(topic, payload)
+  admin.messaging().sendToTopic(topic, payload, options)
     .then(function(response) {
       console.log("Successfully sent message:", response);
     })
