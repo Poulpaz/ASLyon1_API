13921d58e98d69bcfb6d43d3e8f9415674294646
Tristan <letristandu39@hotmail.fr>
2019-01-17 11:48:47 +0100

    [TASK] add route subscription_tournament.

--- controller/subscribe_tournamentController.js
+++ controller/subscribe_tournamentController.js
@@ -10,44 +10,26 @@ var connectionOnline = mysql.createConnection({
 //Lister toutes les inscriptions à un tournoi
 exports.subscribe_tournamentPlayers = function(req, res, next) {
     var idTournament = req.params.idTournament;
-    connectionOnline.query("SELECT user.idUser, user.lastname, user.firstname, team.idTeam, team.teamName FROM user, tournament, team WHERE team.user_idUser = user.idUser AND tournament.idTournament= '" + idTournament + "'", function(err, result, fields) {
+    connectionOnline.query("SELECT user.idUser, user.lastname, user.firstname, tournament.idTournament, tournament.title, team.idTeam, team.teamName, teamPlayers.lastnameSubscriber, teamPlayers.firstnameSubscriber FROM subscribe_tournament, user, tournament, team, teamPlayers WHERE team.user_idUser = user.idUser AND teamPlayers.team_idTeam = team.idTeam AND subscribe_tournament.team_idTeam = team.idTeam AND subscribe_tournament.tournament_idTournament = tournament.idTournament AND tournament.idTournament= '" + idTournament + "'", function(err, result, fields) {
         if(err) { throw err; }
-        else {
-            Object.keys(result).forEach(function (key) {
-                rowIdTeam = result[key].idTeam;
-                data += getPlayersInTeam(rowIdTeam);
-            });
-            res.json(data);
-        }
+        else { res.json(result); }
     });
-    // var idTournament = req.params.idTournament;
-    // connectionOnline.query("SELECT user.idUser, user.lastname, user.firstname, tournament.idTournament, tournament.title, team.idTeam, team.teamName, teamPlayers.lastnameSubscriber, teamPlayers.firstnameSubscriber FROM subscribe_tournament, user, tournament, team, teamPlayers WHERE team.user_idUser = user.idUser AND teamPlayers.team_idTeam = team.idTeam AND subscribe_tournament.team_idTeam = team.idTeam AND subscribe_tournament.tournament_idTournament = tournament.idTournament AND tournament.idTournament= '" + idTournament + "'", function(err, result, fields) {
-    //     if(err) { throw err; }
-    //     else { res.json(result); }
-    // });
 }
 
 //Lister toutes les équipes inscrites à un tournoi
-exports.subscribe_tournamentTeams = function(req, res, next, data) {
+exports.subscribe_tournamentTeams = function(req, res, next) {
     var idTournament = req.params.idTournament;
     connectionOnline.query("SELECT user.idUser, user.lastname, user.firstname, team.idTeam, team.teamName FROM user, tournament, team WHERE team.user_idUser = user.idUser AND tournament.idTournament= '" + idTournament + "'", function(err, result, fields) {
         if(err) { throw err; }
-        else {
-            Object.keys(result).forEach(function (key) {
-                rowIdTeam = result[key].idTeam;
-                data += getPlayersInTeam(rowIdTeam);
-            });
-            res.json(data);
-        }
+        else { res.json(result); }
     });
 }
 
-//Récupérer les joueurs d'une équipe
-function getPlayersInTeam(idTeam) {
-    connectionOnline.query("SELECT user.idUser, user.lastname, user.firstname, teamPlayers.lastnameSubscriber, teamPlayers.firstnameSubscriber FROM user, team, teamPlayers WHERE team.user_idUser = user.idUser AND teamPlayers.team_idTeam = team.idTeam AND team.idTeam= '" + idTeam + "'", function (err, result, fields) {
-        if (err) { throw err; }
-        else {
-            return result;
-        }
+//Lister les joueurs d'une équipe
+exports.subscribe_tournamentTeamsPlayers = function(req, res, next) {
+    var idTeam = req.params.idTeam;
+    connectionOnline.query("SELECT user.idUser, user.lastname, user.firstname, teamPlayers.lastnameSubscriber, teamPlayers.firstnameSubscriber FROM user, team, teamPlayers WHERE team.user_idUser = user.idUser AND teamPlayers.team_idTeam = team.idTeam AND team.idTeam= '" + idTeam + "'", function(err, result, fields) {
+        if(err) { throw err; }
+        else { res.json(result); }
     });
 }
