2d9963c79a74723774afae383079be434a49f898
Tristan <letristandu39@hotmail.fr>
2019-01-17 14:36:06 +0100

    [TASK] add route subscription_tournament.

--- controller/subscribe_tournamentController.js
+++ controller/subscribe_tournamentController.js
@@ -28,8 +28,32 @@ exports.subscribe_tournamentTeams = function(req, res, next) {
 //Lister les joueurs d'une équipe
 exports.subscribe_tournamentTeamsPlayers = function(req, res, next) {
     var idTeam = req.params.idTeam;
-    connectionOnline.query("SELECT user.idUser, user.lastname, user.firstname, teamPlayers.idTeamPlayers, teamPlayers.lastnameSubscriber, teamPlayers.firstnameSubscriber FROM user, team, teamPlayers WHERE team.user_idUser = user.idUser AND teamPlayers.team_idTeam = team.idTeam AND team.idTeam= '" + idTeam + "'", function(err, result, fields) {
+    connectionOnline.query("SELECT teamPlayers.idTeamPlayers, teamPlayers.lastnameSubscriber, teamPlayers.firstnameSubscriber FROM user, team, teamPlayers WHERE team.user_idUser = user.idUser AND teamPlayers.team_idTeam = team.idTeam AND team.idTeam= '" + idTeam + "'", function(err, result, fields) {
         if(err) { throw err; }
         else { res.json(result); }
     });
+}
+
+//Création de l'équipe
+function createTeam(idUser, teamName) {
+    connectionOnline.query("INSERT INTO team (teamName, user_idUser) VALUES ('" + idUser + "', '" + teamName + "')", function (err, result, fields) {
+        if (err) { throw err; }
+        else { }
+    });
+}
+
+//S'inscrire à un tournoi
+exports.addSubscribe_tournament = function(req, res, next) {
+    //var teamName = req.body.teamName;
+    //var user_idUser = req.body.user_idUser;
+    var listTeamPlayers = req.body.listTeamPlayers;
+    //var tournament_idTournament = req.body.tournament_idTournament;
+    //createTeam(user_idUser, teamName);
+    Object.keys(listTeamPlayers).forEach(function (key) {
+        var lastnameSubscriber = result[key].lastnameSubscriber;
+        var firstnameSubscriber = result[key].firstnameSubscriber;
+        console.log(lastnameSubscriber + " " + firstnameSubscriber);
+        //insertPlayerIntoDB(lastnameSubscriber, firstnameSubscriber);
+    });
+    res.json( { message: document } );
 }
