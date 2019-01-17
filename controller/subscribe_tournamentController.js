8056daf6d58d80e3064677342860f78162846b48
Tristan <letristandu39@hotmail.fr>
2019-01-17 10:51:42 +0100

    [TASK] add route subscription_tournament.

--- controller/subscribe_tournamentController.js
+++ controller/subscribe_tournamentController.js
@@ -10,7 +10,7 @@ var connectionOnline = mysql.createConnection({
 //Lister toutes les inscriptions à un tournoi
 exports.subscribe_tournament = function(req, res, next) {
     var idTournament = req.params.idTournament;
-    connectionOnline.query("SELECT user.idUser, user.lastname, user.firstname, tournament.idTournament, tournament.title, team.idTeam, teamPlayers.lastname, teamPlayers.firstname FROM subscribe_tournament, user, tournament, team, teamPlayers WHERE team.user_idUser = user.idUser AND teamPlayers.team_idTeam = team.idTeam AND subscribe_tournament.team_idTeam = team.idTeam AND subscribe_tournament.tournament_idTournament = tournament.idTournament AND tournament.idTournament= '" + idTournament + "'", function(err, result, fields) {
+    connectionOnline.query("SELECT user.idUser, user.lastname, user.firstname, tournament.idTournament, tournament.title, team.idTeam, teamPlayers.lastnameSubscriber, teamPlayers.firstnameSubscriber FROM subscribe_tournament, user, tournament, team, teamPlayers WHERE team.user_idUser = user.idUser AND teamPlayers.team_idTeam = team.idTeam AND subscribe_tournament.team_idTeam = team.idTeam AND subscribe_tournament.tournament_idTournament = tournament.idTournament AND tournament.idTournament= '" + idTournament + "'", function(err, result, fields) {
         if(err) { throw err; }
         else { res.json(result); }
     });
