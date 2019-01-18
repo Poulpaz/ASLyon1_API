e0b3475da66ea972dd30078f434fa659b2eb61d6
Tristan <letristandu39@hotmail.fr>
2019-01-18 09:18:22 +0100

    [FEAT] create connexion independant.

--- connexion/connexion.js
+++ connexion/connexion.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql');
 
 //Récupérer la connexion
-exports.getConnexion = mysql.createConnection({
-    // properties
-    host: 'db4free.net',
-    user: 'aslyon1',
-    password: 'cWqjRnYLFv84Vkr',
-    database: 'aslyon1_api'
-});
+function getConnexion() {
+    return mysql.createConnection({
+        // properties
+        host: 'db4free.net',
+        user: 'aslyon1',
+        password: 'cWqjRnYLFv84Vkr',
+        database: 'aslyon1_api'
+    });
+}
+
+module.exports = { getConnexion() };
